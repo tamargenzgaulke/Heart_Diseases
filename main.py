cadc949032dca5bc6042f8e2f4227794b98a1c44
tamargenzgaulke <tgenzgaulke@gmail.com>
2025-02-12 06:32:11 -0300

    First

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import shap
+import matplotlib.pyplot as plt
+
+# Carregar o dataset
+df = pd.read_csv("/Users/tamargenzgaulke/Documents/GitHub/Heart_Diseases/heart_disease_uci.csv") 
+
